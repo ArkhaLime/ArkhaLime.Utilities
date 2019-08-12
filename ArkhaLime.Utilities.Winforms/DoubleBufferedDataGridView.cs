4d8904d9068357dbfccab3b3fafdf5f0b6b083f8
Michel Fiegel <fiegelmichel@outlook.com>
2019-08-12 21:13:07 +0200

    Création projet pour Winforms
    Ajout classe DoubleBufferedDataGridView

---
+++ ArkhaLime.Utilities.Winforms/DoubleBufferedDataGridView.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArkhaLime.Utilities.Winforms
+{
+    class DoubleBufferedDataGridView:DataGridView
+    {
+
+        public DoubleBufferedDataGridView() : base() { }
+
+        public new bool DoubleBuffered
+        {
+            get
+            {
+                return base.DoubleBuffered;
+            }
+            set
+            {
+                // if not remote desktop session then enable double-buffering optimization
+                if (SystemInformation.TerminalServerSession)
+                {
+                    base.DoubleBuffered = false;
+                }
+                else
+                {
+                    base.DoubleBuffered = value;
+                }
+
+            }
+        }
+
+    }
+}
