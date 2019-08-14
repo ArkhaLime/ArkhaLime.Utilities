29a1ff13a8b403b850011fee5402d1afa5773824
MF <michel.fiegel@merckel.fr>
2019-08-14 15:46:49 +0200

    Ajout projet pour extensions et extension pour List<T>

---
+++ ArkhaLime.Utilities.Extensions/ListExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArkhaLime.Utilities.Extensions
+{
+    public static class ListExtensions
+    {
+
+        public static string Join<T>(this List<T> list, string separator)
+        {
+            if (list == null) return "";
+            return string.Join<T>(separator, list);
+        }
+
+    }
+}
